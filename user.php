e9ebaa32672864d6f06f58d1144798c283cd2924
StereoZing <94567076+StereoZing@users.noreply.github.com>
2025-05-26 11:51:34 +0300

    Add files via upload

--- user.php
+++ user.php
@@ -6,6 +6,7 @@
     <title>Личный кабинет</title>
     <link rel="icon" type="image/png" href="sourse/img/Logo1.png">
     <link rel="stylesheet" href="/SummerPractice2/css/style.css">
+    <link rel="stylesheet" href="/SummerPractice2/css/style2.css">
 </head>
 <body>
 
@@ -21,14 +22,16 @@
         </div>
 
         <div class="container">
-            <?php
-                if(isset($_COOKIE['login'])){
-                    echo '<button onclick="window.location.href=\'/SummerPractice2/lib/logout.php\'">Выйти</button>';
-                } else {
-                    echo '<button onclick="window.location.href=\'/SummerPractice2/register.php\'">Регистрация</button>';
-                    echo '<button onclick="window.location.href=\'/SummerPractice2/enter.php\'">Вход</button>';
-                }
-            ?>
+            <div class='regOrLogin'>
+                <?php
+                    if(isset($_COOKIE['login'])){
+                        echo '<button onclick="window.location.href=\'/SummerPractice2/lib/logout.php\'">Выйти</button>';
+                    } else {
+                        echo '<button onclick="window.location.href=\'/SummerPractice2/register.php\'">Регистрация</button>';
+                        echo '<button onclick="window.location.href=\'/SummerPractice2/enter.php\'">Вход</button>';
+                    }
+                ?>
+            </div>
         </div>
     </main>
 
